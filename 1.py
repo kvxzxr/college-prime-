fbec3cb71005800b0ba729c3e56c9e8c929f9f49
kvxzxr <ivanuk596@gmail.com>
2024-10-01 10:48:40 +0300

    1.py

---
+++ 1.py
@@ -0,0 +1,9 @@
+name = "Назар"
+surname = "Іванюк"
+age = 16
+wrld = "Hello World"
+
+print("Тип name:",type(name))
+print("Тип surname:", type(surname))
+print("Тип age:" ,type(age))
+print("Тип wrld:", type(wrld))
